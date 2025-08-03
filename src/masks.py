79b06d8575afdc35121947d1e086ee1e3f010aca
Eugene Zorin <militarydriverb@gmail.com>
2025-08-03 22:05:44 +0300

    files update

--- src/masks.py
+++ src/masks.py
@@ -11,6 +11,7 @@ def get_mask_card_number(card_number: int) -> str:
 
 if __name__ == "__main__":
     print(get_mask_card_number(1234567890123456))
+    print(get_mask_card_number(123456789012345))
 
 
 def get_mask_account(account_number: int) -> str:
@@ -25,3 +26,4 @@ def get_mask_account(account_number: int) -> str:
 
 if __name__ == "__main__":
     print(get_mask_account(1234567890123456))
+    print(get_mask_account(12345678901234))
--- src/processing.py
+++ src/processing.py
@@ -3,8 +3,7 @@ from datetime import datetime
 
 def filter_by_state(list_of_dict: list, state='EXECUTED') -> list:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
-    state
-     соответствует указанному значению"""
+    state соответствует указанному значению"""
     filtered_list = []
     for dict_item in list_of_dict:
         if dict_item.get('state') == state:
