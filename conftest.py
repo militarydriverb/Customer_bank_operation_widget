47fd606d69ec52e5c85ef84e2af7534b90aa6f4a
Eugene Zorin <militarydriverb@gmail.com>
2025-08-03 14:38:40 +0300

    updates adding

---
+++ conftest.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.fixture
+def fixture_card_number_str():
+    return str(card_number)
--- src/masks.py
+++ src/masks.py
@@ -2,15 +2,23 @@ def get_mask_card_number(card_number: int) -> str:
     """Функция принимает на вход номер карты в виде
     числа и возвращает маску номера по правилу
     XXXX XX** **** XXXX."""
-    card_number_str = str(card_number)
-    if len(card_number_str) == 16:
-        mask_number = f"{card_number_str[:4]} {card_number_str[4:6]} ** **** {card_number_str[12:]}"
-        return mask_number
-    return "Некорректный ввод"
+    if card_number is None:
+        return "Неверный тип данных"
+    else:
+        card_number_str = str(card_number)
+        if not card_number_str.isnumeric():
+            return "Неверный номер карты"
+        if len(card_number_str) == 16:
+            mask_number = f"{card_number_str[:4]} {card_number_str[4:6]} ** **** {card_number_str[12:]}"
+            return mask_number
+        return "Неверный номер карты"
 
 
 if __name__ == "__main__":
     print(get_mask_card_number(1234567890123456))
+    print(get_mask_card_number(123456789012345))
+    print(get_mask_card_number("700079ffffffff"))
+    print(get_mask_card_number(""))
 
 
 def get_mask_account(account_number: int) -> str:
