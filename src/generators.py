be3f797002af5499ba578a981de22cede66e4efe
Eugene Zorin <militarydriverb@gmail.com>
2025-08-10 22:05:26 +0300

    module generators: code line of block __name__ == "__main__" excluded from test coverage, flake8 and mypy checks passed module test_generators: docstrings added for tests the Readme file: new funcs description added test coverage: run, requested level of test coverage reached

--- src/generators.py
+++ src/generators.py
@@ -4,9 +4,6 @@ from typing import Iterator
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
     """Фильтрует транзакции по заданной валюте и возвращает итератор."""
     return filter(lambda x: x.get("operationAmount", {}).get("currency", {}).get("code") == currency, transactions)
-    # for transaction in transactions:
-    #     if transaction["operationAmount"]["currency"]["code"] == currency:
-    #         yield transaction
 
 
 def transaction_descriptions(transactions: list) -> Iterator[dict]:
@@ -15,12 +12,6 @@ def transaction_descriptions(transactions: list) -> Iterator[dict]:
     for i in transactions:
         yield i["description"]
 
-    # Перевод организации
-    # Перевод со счета на счет
-    # Перевод со счета на счет
-    # Перевод с карты на карту
-    # Перевод организации
-
 
 def card_number_generator(start: int, stop: int):
     """ Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты.
@@ -28,97 +19,97 @@ def card_number_generator(start: int, stop: int):
     Генератор должен принимать начальное и конечное значения для генерации диапазона номеров."""
     if start < 1 or stop > 9999999999999999:
         raise ValueError('Число не может быть меньше 1 и больше 9999999999999999')
-    for i in range(start, stop+1):
+    for i in range(start, stop + 1):
         card_number = str(i).zfill(16)
         yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
 
 
 if __name__ == "__main__":
-    transactions = [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-    # Фильтрация транзакций с валютой USD
-    usd_transactions = filter_by_currency(transactions, "USD")
-    for _ in range(2):
-        print(next(usd_transactions))
-
-    descriptions = transaction_descriptions(transactions)
-    for _ in range(5):
-        print(next(descriptions))
-
-    for card_number in card_number_generator(0, 5):
-        print(card_number)
+    transactions = [                                    # pragma: no cover type: ignore # noqa
+        {                                               # pragma: no cover type: ignore # noqa
+            "id": 939719570,                            # pragma: no cover type: ignore # noqa
+            "state": "EXECUTED",                        # pragma: no cover type: ignore # noqa
+            "date": "2018-06-30T02:08:58.425572",       # pragma: no cover type: ignore # noqa
+            "operationAmount": {                        # pragma: no cover type: ignore # noqa
+                "amount": "9824.07",                    # pragma: no cover type: ignore # noqa
+                "currency": {                           # pragma: no cover type: ignore # noqa
+                    "name": "USD",                      # pragma: no cover type: ignore # noqa
+                    "code": "USD"                       # pragma: no cover type: ignore # noqa
+                }                                       # pragma: no cover type: ignore # noqa
+            },                                          # pragma: no cover type: ignore # noqa
+            "description": "Перевод организации",       # pragma: no cover type: ignore # noqa
+            "from": "Счет 75106830613657916952",        # pragma: no cover type: ignore # noqa
+            "to": "Счет 11776614605963066702"           # pragma: no cover type: ignore # noqa
+        },                                              # pragma: no cover type: ignore # noqa
+        {                                               # pragma: no cover type: ignore # noqa
+            "id": 142264268,                            # pragma: no cover type: ignore # noqa
+            "state": "EXECUTED",                        # pragma: no cover type: ignore # noqa
+            "date": "2019-04-04T23:20:05.206878",       # pragma: no cover type: ignore # noqa
+            "operationAmount": {                        # pragma: no cover type: ignore # noqa
+                "amount": "79114.93",                   # pragma: no cover type: ignore # noqa
+                "currency": {                           # pragma: no cover type: ignore # noqa
+                    "name": "USD",                      # pragma: no cover type: ignore # noqa
+                    "code": "USD"                       # pragma: no cover type: ignore # noqa
+                }                                       # pragma: no cover type: ignore # noqa
+            },                                          # pragma: no cover type: ignore # noqa
+            "description": "Перевод со счета на счет",  # pragma: no cover type: ignore # noqa
+            "from": "Счет 19708645243227258542",        # pragma: no cover type: ignore # noqa
+            "to": "Счет 75651667383060284188"           # pragma: no cover type: ignore # noqa
+        },                                              # pragma: no cover type: ignore # noqa
+        {                                               # pragma: no cover type: ignore # noqa
+            "id": 873106923,                            # pragma: no cover type: ignore # noqa
+            "state": "EXECUTED",                        # pragma: no cover type: ignore # noqa
+            "date": "2019-03-23T01:09:46.296404",       # pragma: no cover type: ignore # noqa
+            "operationAmount": {                        # pragma: no cover type: ignore # noqa
+                "amount": "43318.34",                   # pragma: no cover type: ignore # noqa
+                "currency": {                           # pragma: no cover type: ignore # noqa
+                    "name": "руб.",                     # pragma: no cover type: ignore # noqa
+                    "code": "RUB"                       # pragma: no cover type: ignore # noqa
+                }                                       # pragma: no cover type: ignore # noqa
+            },                                          # pragma: no cover type: ignore # noqa
+            "description": "Перевод со счета на счет",  # pragma: no cover type: ignore # noqa
+            "from": "Счет 44812258784861134719",        # pragma: no cover type: ignore # noqa
+            "to": "Счет 74489636417521191160"           # pragma: no cover type: ignore # noqa
+        },                                              # pragma: no cover type: ignore # noqa
+        {                                               # pragma: no cover type: ignore # noqa
+            "id": 895315941,                            # pragma: no cover type: ignore # noqa
+            "state": "EXECUTED",                        # pragma: no cover type: ignore # noqa
+            "date": "2018-08-19T04:27:37.904916",       # pragma: no cover type: ignore # noqa
+            "operationAmount": {                        # pragma: no cover type: ignore # noqa
+                "amount": "56883.54",                   # pragma: no cover type: ignore # noqa
+                "currency": {                           # pragma: no cover type: ignore # noqa
+                    "name": "USD",                      # pragma: no cover type: ignore # noqa
+                    "code": "USD"                       # pragma: no cover type: ignore # noqa
+                }                                       # pragma: no cover type: ignore # noqa
+            },                                          # pragma: no cover type: ignore # noqa
+            "description": "Перевод с карты на карту",  # pragma: no cover type: ignore # noqa
+            "from": "Visa Classic 6831982476737658",    # pragma: no cover type: ignore # noqa
+            "to": "Visa Platinum 8990922113665229"      # pragma: no cover type: ignore # noqa
+        },                                              # pragma: no cover type: ignore # noqa
+        {                                               # pragma: no cover type: ignore # noqa
+            "id": 594226727,                            # pragma: no cover type: ignore # noqa
+            "state": "CANCELED",                        # pragma: no cover type: ignore # noqa
+            "date": "2018-09-12T21:27:25.241689",       # pragma: no cover type: ignore # noqa
+            "operationAmount": {                        # pragma: no cover type: ignore # noqa
+                "amount": "67314.70",                   # pragma: no cover type: ignore # noqa
+                "currency": {                           # pragma: no cover type: ignore # noqa
+                    "name": "руб.",                     # pragma: no cover type: ignore # noqa
+                    "code": "RUB"                       # pragma: no cover type: ignore # noqa
+                }                                       # pragma: no cover type: ignore # noqa
+            },                                          # pragma: no cover type: ignore # noqa
+            "description": "Перевод организации",       # pragma: no cover type: ignore # noqa
+            "from": "Visa Platinum 1246377376343588",   # pragma: no cover type: ignore # noqa
+            "to": "Счет 14211924144426031657"           # pragma: no cover type: ignore # noqa
+        }                                               # pragma: no cover type: ignore # noqa
+    ]                                                   # pragma: no cover type: ignore # noqa
+    # Фильтрация транзакций с валютой USD               # pragma: no cover type: ignore # noqa
+    usd_transactions = filter_by_currency(transactions, "USD")  # pragma: no cover type: ignore # noqa
+    for _ in range(2):                                  # pragma: no cover type: ignore # noqa
+        print(next(usd_transactions))                   # pragma: no cover type: ignore # noqa
+                                                        # pragma: no cover type: ignore # noqa
+    descriptions = transaction_descriptions(transactions)  # pragma: no cover type: ignore # noqa
+    for _ in range(5):                                  # pragma: no cover type: ignore # noqa
+        print(next(descriptions))                       # pragma: no cover type: ignore # noqa
+                                                        # pragma: no cover type: ignore # noqa
+    for card_number in card_number_generator(1, 5):  # pragma: no cover type: ignore # noqa
+        print(card_number)                              # pragma: no cover type: ignore # noqa
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -4,17 +4,19 @@ from src.generators import filter_by_currency, transaction_descriptions, card_nu
 
 
 def test_filter_by_currency(transactions, input_by_currency):
+    """Тестирование корректности фильтра транзакций по заданной валюте"""
     currency, expected = input_by_currency
     assert list(filter_by_currency(transactions, currency)) == expected
-    # for _ in range(2):
-    #     assert next(filter_by_currency(transactions, "USD")) == transactions
 
 
 def test_filter_by_currency_no_currency(transactions):
+    """Тестирование корректности работы функции при отсутствии транзакций в заданной валюте
+    или пустого списка валютны операций"""
     assert list(filter_by_currency(transactions, "EUR")) == []
 
 
 def test_transaction_descriptions(transactions):
+    """Тестирование корректности возврата описания для каждой транзакции"""
     result = list(transaction_descriptions(transactions))
     assert result == [
         "Перевод организации",
@@ -26,6 +28,7 @@ def test_transaction_descriptions(transactions):
 
 
 def test_transaction_description_no_transactions():
+    """Тестирование работы функции в различным количеством транзакций включая пустой список"""
     result = list(transaction_descriptions([]))
     assert result == []
 
@@ -39,17 +42,12 @@ def test_transaction_description_no_transactions():
     ],
 )
 def test_card_number_generator(start, stop, expected):
+    """Тестирование корректности возврата номеров карт в заданном диапазоне и их форматирования"""
     assert list(card_number_generator(start, stop)) == expected
-    # generated_cards = list(card_number_generator(start, stop))
-    #
-    # # Проверяем, что первый и последний номера равны start и stop
-    # first_card = int(generated_cards[0].replace(' ', ''))
-    # last_card = int(generated_cards[-1].replace(' ', ''))
-    #
-    # assert first_card == start, f"Первый номер не равен start: {generated_cards[0]}"
-    # assert last_card == stop, f"Последний номер не равен stop: {generated_cards[-1]}"
+
 
 def test_card_number_generator_digits_number():
+    """Тестирование корректности обработки крайних значений"""
     with pytest.raises(ValueError) as ex_info:
         list(card_number_generator(0, 5))
     assert str(ex_info.value) == 'Число не может быть меньше 1 и больше 9999999999999999'
