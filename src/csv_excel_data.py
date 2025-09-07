a7a4b1db5016decd1342e2ce299a9c296db34abb
Eugene Zorin <militarydriverb@gmail.com>
2025-09-07 21:34:48 +0300

    new module added

---
+++ src/csv_excel_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv('transactions.csv')
+
+def csv_data_reading(df: pd.DataFrame) -> pd.DataFrame:
+    """Функция для считывания финансовых операций из CSV выдает список словарей с транзакциями"""
+
+
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('transactions.csv')
+    print()
